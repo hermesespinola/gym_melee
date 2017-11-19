a505f0f026d267ec0abe54836ccb9264044fd320
Hermes Espínola González <hermesespinola@users.noreply.github.com>
2017-11-19 14:17:50 -0600

    Delete rankings.py

--- rankings.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-def rank_users():
-    client = MongoClient('mongodb://hermes:hermes@info.gda.itesm.mx:27017/melee')
-    db = client['melee']
-    collection = db['users']
-rank_users()
